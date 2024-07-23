1bf1412c436759a1690525d062a69b35adc9c279
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2024-07-23 17:16:59 +0530

    Update App.jsx

--- 02counterproject/src/App.jsx
+++ 02counterproject/src/App.jsx
@@ -1,1 +1,33 @@
+import { useState } from 'react';
+import './App.css'
 
+function App() {
+  let [counter,setCounter] = useState(15);
+  // let counter =5;
+
+  const addValue = ()=>{
+    if(counter == 20) return;
+    counter += 1;
+    setCounter(counter);
+    console.log(Math.random()+" "+ counter);
+  };
+
+  const remValue = ()=>{
+    if(counter == 0) return;
+    counter -=1;
+    setCounter(counter);
+    console.log(counter);
+  };
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={remValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App

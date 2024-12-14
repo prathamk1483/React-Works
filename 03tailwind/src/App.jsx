e54d5766a31767df524ab07d96a10ca722c9f801
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2024-12-14 10:41:05 +0530

    Created new Project

---
+++ 03tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./assets/card"
+
+function App() {
+  
+
+  return (
+    <>
+      <Card userName="Alien_One" rate="0.69" />
+      <Card userName="Alien_Two" rate="6.9" />
+      <Card userName="Alien_Three" rate="69" />
+
+      <h1>This is the Tailwind Component that didnt work</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwind/src/assets/card.jsx
@@ -0,0 +1,34 @@
+function Card(props){
+    return(
+        <div className="flex flex-col rounded-xl  p-4"
+        style={{
+          border: '0.88px solid',
+  
+          backdropFilter: 'saturate(180%) blur(14px)',
+          background: ' #ffffff0d',
+        }}
+      >
+        <div>
+          <img
+            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+            alt="nft-gif"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col  rounded-b-xl py-4 ">
+          <div className="flex justify-between">
+            <h1 className="font-RubikBold ">{props.userName}</h1>
+            <h1 className="font-bold font-RubikBold">Price</h1>
+          </div>
+          <div className="flex  justify-between font-mono">
+            <p>#345</p>
+            <p>{props.rate}</p>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Card;
